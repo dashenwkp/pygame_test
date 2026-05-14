b8873ebd2294e97124c879e2e06016309bebd63c
dashenwkp <2126879457@qq.com>
2026-05-14 20:47:44 +0800

    准备开始做下雨功能

--- ex13_3_rain/star_class.py
+++ ex13_3_rain/star_class.py
@@ -6,7 +6,7 @@ class Star(Sprite):
     def __init__(self, st_game):
         super().__init__()
         '''初始化星星, 并将其rect坐标设为左上角'''
-        self.image = pygame.image.load(r'ex13_2_stars\images\star.png')
+        self.image = pygame.image.load(r'ex13_3_rain\images\raindrop.png')
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
